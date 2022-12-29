f68d6b8d846dc4f3f0264b91fd78b49bddffd772
Str367 <str3670404@gmail.com>
2022-12-29 13:08:26 +0800

    db

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default {
-    connect: () => {
-         mongoose
-            .connect("mongodb://mongo:GlkGmHkud2GMB27NWTHE@containers-us-west-22.railway.app:6934", {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            .then((res) => console.log("mongo db connection created"));
-    }
+  connect: () => {
+    mongoose
+      .connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((res) => console.log("mongo db connection created"));
+  },
 };
