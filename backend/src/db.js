8cbe488a5b1a351d7454f771c4a95afabf3cc35d
Str367 <str3670404@gmail.com>
2022-12-29 13:20:11 +0800

    db

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 export default {
   connect: () => {
     mongoose
-      .connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(
+        "mongodb+srv://Str367:Aron55668899@strsclusterforwp.gsdiboi.mongodb.net/?retryWrites=true&w=majority",
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
       .then((res) => console.log("mongo db connection created"));
   },
 };
