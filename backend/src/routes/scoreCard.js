46444a59fac33b59010346ce7412e6c0c308c5b1
Str367 <str3670404@gmail.com>
2022-12-29 15:03:11 +0800

    scorecard

--- backend/src/routes/scoreCard.js
+++ backend/src/routes/scoreCard.js
@@ -1,72 +1,58 @@
 import { Router } from "express";
-import ScoreCard from "../models/ScoreCard";
-import express from "express";
-
+import User from "../models/user";
 const router = Router();
-router.use(express.json());
-
-const deleteDB = async () => {
+router.post("/card", async (req, res) => {
+  let { name, subject, score } = req.body;
+  const existing = await User.findOne({ name: name, subject: subject });
+  if (existing) {
+    res.json({
+      message: "Updating (" + name + ", " + subject + ", " + score + ")",
+    });
     try {
-        await ScoreCard.deleteMany({});
-    } catch (e) { throw new Error("Database deletion failed"); }
-};
-
-router.delete("/cards", (_ , res) => {
-    deleteDB()
-    res.json({message : "Database cleared"})
+      await User.updateOne(
+        { name: name, subject: subject },
+        { $set: { score: score } }
+      );
+    } catch (e) {
+      throw new Error("User creation error: " + e);
+    }
+  } else {
+    const newUser = new User({ name, subject, score });
+    res.json({
+      message: "Adding (" + name + ", " + subject + ", " + score + ")",
+    });
+    return newUser.save();
+  }
 });
 
-let existing = true
-const saveData = async (name, subject, score) => {
-    existing = await ScoreCard.findOne({ name, subject });
-    if (existing) {
-        await ScoreCard.deleteOne({ name, subject })
-    }
-    try {
-        const newData = new ScoreCard({ name, subject, score });
-        return newData.save();
-    } catch (e) { throw new Error("User creation error: " + e); }
-};
-
-router.post("/card", (req , res) => {
-    const name = req.body.name
-    const subject = req.body.subject
-    const score = req.body.score;
-    
-    (async () => {
-        await saveData(name, subject, score)
-        res.json({message : existing ? 
-            `Updating:{name:${name}, subject:${subject}, score:${score}}` :
-            `Adding:{name:${name}, subject:${subject}, score:${score}}` , card:true})
-    })();
+router.delete("/cards", async (req, res) => {
+  try {
+    await User.deleteMany();
+    res.json({ messages: [], message: "Database cleared" });
+  } catch (e) {
+    throw new Error("Database deletion failed");
+  }
 });
-
-router.get("/cards", (req , res) => {
-    const type = req.query.type
-    const val = req.query.queryString
-
-    if (type == 'name'){
-        (async () => {
-            const matchData = await ScoreCard.find({ name:val })
-            res.json({
-                messages : (matchData.length>0) ?
-                    matchData.map(e => `Found card with name: (${e.name}, ${e.subject}, ${e.score})`) : 
-                    false,
-                message : `Name (${val}) not found!`
-            })
-        })();
-    }
-    else {
-        (async () => {
-            const matchData = await ScoreCard.find({ subject:val })
-            res.json({
-                messages : (matchData.length>0) ?
-                    matchData.map(e => `Found card with subject: (${e.name}, ${e.subject}, ${e.score})`) : 
-                    false,
-                message : `Subject (${val}) not found!`
-            })
-        })();
-    }
+router.get("/cards", async (req, res) => {
+  await console.log(req.query);
+  let { type, queryString } = req.query;
+  let returnedSet;
+  if (type === "name") returnedSet = await User.find({ name: queryString });
+  else returnedSet = await User.find({ subject: queryString });
+  if (returnedSet.length == 0)
+    res.json({ message: type + " ( " + queryString + " ) not found!" });
+  else
+    res.json({
+      messages: returnedSet.map(
+        (data) =>
+          "Found card with subject: (" +
+          data.name +
+          ", " +
+          data.subject +
+          ", " +
+          data.score +
+          ")."
+      ),
+    });
 });
-
 export default router;
