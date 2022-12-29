ee70d3183b149d5d08ca2ea95532ff3a47883a47
Str367 <str3670404@gmail.com>
2022-12-29 10:04:37 +0800

    test

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import cors from 'cors';
+import routes from './routes';
+import db from './db';
+import path from "path";
+import express from "express";
+
+db.connect();
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
+// define routes
+app.use('/', routes);
+
+// define server
+const port = process.env.PORT || 4000;
+
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+);
